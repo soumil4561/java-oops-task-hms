1aee8333cf9383666182d0f17439888d6c260244
Soumil Singh <soumil.sps@gmail.com>
2025-02-09 23:29:22 +0530

    added staff abstraction

---
+++ src/main/java/org/example/models/Staff/Staff.java
@@ -0,0 +1,32 @@
+package org.example.models.Staff;
+
+public abstract class Staff {
+    private final String id;
+    private final String name;
+    private final String role;
+
+    public Staff(String id, String name, String role) {
+        this.id = id;
+        this.name = name;
+        this.role = role;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public abstract void performDuties(); // Abstract method (to be implemented by subclasses)
+
+    @Override
+    public String toString() {
+        return String.format("| %-3d | %-15s | %-12s |", id, name, role);
+    }
+}
