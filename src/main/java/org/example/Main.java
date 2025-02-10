0f605fc148f9f2f6acca055092c98b4579af0478
Soumil Singh <soumil.sps@gmail.com>
2025-02-10 10:57:39 +0530

    updated Main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,11 @@
 package org.example;
 
+import org.example.models.Hospital.Hospital;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Hospital hospital = new Hospital();
+        hospital.start();
     }
 }
+
